579fdac18f7f707874458914abfbec066b273964
Eric Dostie <dostieer@msu.edu>
2012-06-23 17:12:56 -0400

    Challenge 3 done and it's readme updated. Also preserving original bhaarat.txt in git repository.

--- 3/file.py
+++ 3/file.py
@@ -5,13 +5,14 @@
 # file.py
 #
 # Reads a languages file, does some parsing, writes an output file.
+# Numbers the output file starting from 0. i.e. 0, 1, 2, etc.
 # Then adds more text to source languages file.
 
 def main():
 	try:
-		f = open('bhaarat.txt', 'r')
+		f = open('src.txt', 'r')
 	except IOError:
-		print "Could not open \"bhaarat.txt\"."
+		print "Could not open \"src.txt\"."
 
 	try:
 		newFile = open('out.txt', 'w')
@@ -19,12 +20,24 @@ def main():
 		print "Could not open \"out.txt\"."
 		
 	lineNumber = 0
+	totalLines = 0
 	for line in f:
+		totalLines += 1
 		language = line.split('.')[1].strip()
 		if language[0] == "H" or language[0] == "S":
-			print "Appending language to output file: %s" % language
+			# print "Appending language to output file: %s" % language
+			newFile.write("%d %s\n" % (lineNumber, language))
 			lineNumber += 1
-			newFile.write("%d %s" % lineNumber, language)
+	
+	f.close()
+	newFile.close()
+
+	# let's do a one liner to append to the original source file
+	# re: slavik -- yes, what IS the point of this? 
+	# appending a language that won't affect repeated executions of the program?
+	# todo? -- reference a list of H/S languages and append a *new* one to src lang. list
+	open('src.txt', 'a').write("%d. English\n" % (totalLines+1))
 
 if __name__ == "__main__":
 	main()
+	print "Done!"
--- 3/readme.md
+++ 3/readme.md
@@ -4,3 +4,8 @@
 Write a program that reads "bhaarat.txt", and takes the languages that begin with "H" or "S" and writes them to another file, named "out.txt". Then have the program write "23. English" to the end to continue the list in "bhaarat.txt".
 
 <a href="http://ubuntuforums.org/showthread.php?t=884394">http://ubuntuforums.org/showthread.php?t=884394</a>
+
+## Notes
+---------
+
+I've omitted some working files from the git repository to preserve the original source text in the repository for others looking to fork this. This is why my source code targets a file called src.txt.
